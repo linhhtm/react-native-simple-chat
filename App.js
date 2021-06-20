f3dfec5141cb7137ecfddb794e4c4fea05fb1fe5
louise <louise@welinc.vn>
2021-06-21 02:37:55 +0700

    implement simple chat + change cluster

--- App.js
+++ App.js
@@ -1,21 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import SocketIOClient from 'socket.io-client';
+import { GiftedChat } from 'react-native-gifted-chat';
+import AsyncStorage from '@react-native-community/async-storage';
+const USER_ID = '@userId';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      userId: null,
+    }
+    
+    this.determineUser = this.determineUser.bind(this);
+    this.onReceivedMessage = this.onReceivedMessage.bind(this);
+    this.onSend = this.onSend.bind(this);
+    this._storeMessages = this._storeMessages.bind(this);
+    this.socket = SocketIOClient('http://localhost:3000');
+    this.socket.on('message', this.onReceivedMessage);
+    this.determineUser();
+  };
+
+  determineUser() {
+    AsyncStorage.getItem(USER_ID).then((userId) => {
+      if (!userId) {
+        
+        this.socket.emit('userJoined', null);
+        this.socket.on('userJoined', (userId) => {
+          AsyncStorage.setItem(USER_ID, userId);
+          this.setState({userId})
+        })
+      } else {
+        this.socket.emit('userJoined', userId);
+        this.setState({userId})
+      }
+    }).catch((err) => alert(e))
+  }
+
+  onReceivedMessage(messages) {
+    this._storeMessages(messages);
+  }
+
+  onSend(messages = []) {
+    this.socket.emit('message', messages[0]);
+    this._storeMessages(messages);
+  }
+
+  _storeMessages(messages) {
+    this.setState(prevState => {
+      return {
+        messages: GiftedChat.append(prevState.messages, messages)
+      }
+    })
+  }
+  
+  render() {
+    var user = {
+      _id: this.state.userId || -1
+    }
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={this.onSend}
+        user={user}
+      />
+    )
+  }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Main
